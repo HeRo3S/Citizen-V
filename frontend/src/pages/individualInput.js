f4e61f019bf4bc044c9c5e39f2a8ddfbedd92782
HeRo3S <hungid6a1@gmail.com>
2021-12-25 23:12:59 +0700

    Add form validation for individualInput

--- frontend/src/pages/individualInput.js
+++ frontend/src/pages/individualInput.js
@@ -1,35 +1,44 @@
 import React, { useState } from 'react'
+import {Formik, Form, Field, ErrorMessage} from 'formik'
+import * as Yup from 'yup'
 import userServices from '../services/user.services';
 import './individualInput.css'
 import Layout from './layout';
 
 function IndividualInput(){
     const [data, setData] = useState({
-        code: '1234567890',
-        name:"Nguyễn Văn A",
-        birthday: "06/09/1969",
-        gender: "Nam",
-        profession: "Streamer",
-        religion: "Không",
-        education:"Không",
+        code: "",
+        name: "",
+        birthday: "",
+        gender: "",
+        profession: "",
+        religion: "",
+        education:"",
         origin_address:"",
-        temporary_address:"Không",
+        temporary_address:"",
         permanent_address:""
     })
 
-    //form data change function
-    const handleFormDataChange = (event) => {
-        const fieledName = event.target.getAttribute('name');
-        const fieldValue = event.target.value;
+    //Validation schema
+    const validationSchema = Yup.object().shape({
+        code: Yup.string().required(),
+        name: Yup.string().required(),
+        birthday: Yup.date().required(),
+        gender: Yup.string().required(),
+        profession: Yup.string().required(),
+        religion: Yup.string().required("Please choose a religion"),
+        education: Yup.string().required("Please choose an education degree"),
+        origin_address: Yup.string().required(),
+        temporary_address: Yup.string().required(),
+        permanent_address: Yup.string().required(),
+    });
 
-        const addData = {...data};
-        addData[fieledName] = fieldValue;
-        setData(addData);
-    }
+    const renderError = (message) => <p className='help is-danger'>{message}</p>
 
     //submit data click function
-    const handleFormSubmitClick = () => {
-        userServices.postIndividualInputData(data).then(() => {
+    const handleFormSubmitClick = (value) => {
+        alert('OK! Submitting');
+        userServices.postIndividualInputData(value).then(() => {
             window.location.reload(false);
         })
     }
@@ -39,66 +48,95 @@ function IndividualInput(){
             <Layout />
             <div className="main" id="inputdata-page">
                 <div className="container" id="inputdata-container">
-                    <form onSubmit={handleFormSubmitClick}>
+
+                    <Formik initialValues={data} validationSchema={validationSchema} onSubmit={(value) => handleFormSubmitClick(value)} >
+
+                    <Form>
                         <div className="inputdata-form">
                             <div className='inputdata-heading'>
                                 Nhập thông tin cá nhân
                             </div>
 
                             <table> 
-                                <tr>
-                                    <th>Mã CCCD/CMND</th>
-                                    <th>Họ và tên</th>
-                                    <th>Ngày/Tháng/Năm sinh</th>
-                                </tr>
+                                <tbody>
+                                    <tr>
+                                        <th>Mã CCCD/CMND</th>
+                                        <th>Họ và tên</th>
+                                        <th>Ngày/Tháng/Năm sinh</th>
+                                    </tr>
 
-                                <tr>
-                                    <td>  <input type="text" name='code' placeholder="Nhập CCCD/CMND" onChange={handleFormDataChange} />  </td>
-                                    <td>  <input type="text" name='name' placeholder="Nhập họ và tên" onChange={handleFormDataChange}/>  </td>
-                                    <td>  <input type="date" name='birthday' placeholder="Nhập ngày/tháng/năm"/>  </td>
-                                </tr>
+                                    <tr>
+                                        <td>
+                                            <Field type="text" name='code' placeholder="Nhập CCCD/CMND"  />  
+                                            <ErrorMessage name='code' render={renderError} />
+                                        </td>
+                                        <td>  
+                                            <Field type="text" name='name' placeholder="Nhập họ và tên" />
+                                            <ErrorMessage name='name' render={renderError} />
+                                        </td>
+                                        <td>
+                                            <Field type="date" name='birthday' placeholder="Nhập ngày/tháng/năm"/>  
+                                            <ErrorMessage name='birthday' render={renderError} />
+                                        </td>
+                                    </tr>
 
-                                <tr>
-                                    <th>Giới tính</th>
-                                    <th>Nghề nghiệp</th>
-                                    <th>Tôn giáo</th>
-                                    <th>Trình độ học vấn</th>
-                                </tr>
+                                    <tr>
+                                        <th>Giới tính</th>
+                                        <th>Nghề nghiệp</th>
+                                        <th>Tôn giáo</th>
+                                        <th>Trình độ học vấn</th>
+                                    </tr>
 
-                                <tr>
-                                    <td>
-                                        <select type="text" name='gender' placeholder="Nhập giới tính" onChange={handleFormDataChange}>
-                                            <option value="nam">Nam</option>
-                                            <option value="nu">Nữ</option>
-                                        </select>
-                                    </td>
-                                    <td>  <input type="text" name='profession' placeholder="Nhập nghề nghiệp" onChange={handleFormDataChange} />  </td>
-                                    <td>  
-                                        <select name='religion' placeholder="Nhập tôn giáo" onChange={handleFormDataChange} >
-                                            <option value="Phat giao">Phật giáo</option>
-                                            <option value="Thien chua giao">Thiên chúa giáo</option>
-                                            <option value="Kito giao">Kitô giáo</option>
-                                        </select>
-                                    </td>
-                                    <td>
-                                        <select name='education' placeholder="Nhập trình độ học vấn" onChange={handleFormDataChange}>
-                                            <option value="12/12">12/12</option>
-                                            <option value="Dai hoc">Đại học</option>
-                                        </select>
-                                    </td>
-                                </tr>
+                                    <tr>
+                                        <td>
+                                            <Field as="select" name='gender' placeholder="Nhập giới tính" >
+                                                <option value="None"></option>
+                                                <option value="nam">Nam</option>
+                                                <option value="nu">Nữ</option>
+                                            </Field>
+                                        </td>
+                                        <td>
+                                            <Field type="text" name='profession' placeholder="Nhập nghề nghiệp"  />
+                                            <ErrorMessage name='profession' render={renderError} />
+                                        </td>
+                                        <td>  
+                                            <Field as="select" name='religion' placeholder="Nhập tôn giáo"  >
+                                                <option value="None"></option>
+                                                <option value="Phat giao">Phật giáo</option>
+                                                <option value="Thien chua giao">Thiên chúa giáo</option>
+                                                <option value="Kito giao">Kitô giáo</option>
+                                            </Field>
+                                        </td>
+                                        <td>
+                                            <Field as="select" name='education' placeholder="Nhập trình độ học vấn" >
+                                                <option value="None"></option>
+                                                <option value="12/12">12/12</option>
+                                                <option value="Dai hoc">Đại học</option>
+                                            </Field>
+                                        </td>
+                                    </tr>
 
-                                <tr>
-                                    <th>Quê quán</th>
-                                    <th>Địa chỉ tạm trú</th>
-                                    <th>Địa chỉ thường trú</th>
-                                </tr>
+                                    <tr>
+                                        <th>Quê quán</th>
+                                        <th>Địa chỉ tạm trú</th>
+                                        <th>Địa chỉ thường trú</th>
+                                    </tr>
 
-                                <tr>
-                                    <td>  <input type="text" name='origin_address' placeholder="Nhập quê quán" onChange={handleFormDataChange}/>  </td>
-                                    <td>  <input type="text" name='temporary_address' placeholder="Nhập địa chỉ tạm trú" onChange={handleFormDataChange}/>  </td>
-                                    <td>  <input type="text" name='permanent_address' placeholder="Nhập địa chỉ thường trú" onChange={handleFormDataChange}/>  </td>
-                                </tr>
+                                    <tr>
+                                        <td>
+                                            <Field type="text" name='origin_address' placeholder="Nhập quê quán" />  
+                                            <ErrorMessage name='origin_address' render={renderError} />
+                                        </td>
+                                        <td>
+                                            <Field type="text" name='temporary_address' placeholder="Nhập địa chỉ tạm trú" />  
+                                            <ErrorMessage name='temporary_address' render={renderError} />
+                                        </td>
+                                        <td>
+                                            <Field type="text" name='permanent_address' placeholder="Nhập địa chỉ thường trú" />
+                                            <ErrorMessage name='permanent_address' render={renderError} />
+                                        </td>
+                                    </tr>
+                                </tbody>
                             </table>
                             <div className='inputdata-footer' >
                                 <button type="submit">
@@ -106,7 +144,9 @@ function IndividualInput(){
                                 </button>
                             </div>
                         </div>
-                    </form>    
+                    </Form>
+                    </Formik>
+
                 </div>
             </div>
         </>
